4d393a739ed257eb913a2bc65f3f304563763c80
eduardojigub <eduardobrunoit@gmail.com>
2022-07-04 09:01:07 -0300

    feat: adding prestart function

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -9,6 +9,18 @@ namespace StopWatch
         {
             Menu();
         }
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go!");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
         static void Menu()
         {
             Console.Clear();
@@ -20,7 +32,14 @@ namespace StopWatch
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length - 1, 1));
             int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
 
+            if (type == 'm')
+                multiplier = 60;
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
         }
         static void Start(int time)
         {
@@ -35,6 +54,7 @@ namespace StopWatch
             Console.Clear();
             Console.WriteLine("End of StopWatch");
             Thread.Sleep(2500);
+            Menu();
         }
     }
 }
