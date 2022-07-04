2272fa2eb3e19f8ecf1558b33c3c96a43ac591cf
eduardojigub <eduardobrunoit@gmail.com>
2022-07-04 08:41:42 -0300

    feat: adding menu function

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -7,11 +7,18 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Seconds");
+            Console.WriteLine("M = Minutes");
+            Console.WriteLine("0 = Leave StopWatch");
+            Console.WriteLine("How many time you wanna count?");
+        }
+        static void Start(int time)
+        {
             int currentTime = 0;
             while (currentTime != time)
             {
@@ -20,6 +27,9 @@ namespace StopWatch
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+            Console.Clear();
+            Console.WriteLine("End of StopWatch");
+            Thread.Sleep(2500);
         }
     }
 }
