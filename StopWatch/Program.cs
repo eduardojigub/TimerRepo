b955d66edfc1adb3acd930c645b7657c52c83c31
eduardojigub <eduardobrunoit@gmail.com>
2022-07-04 08:49:52 -0300

    feat: adding substring and parse on typed

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -12,10 +12,14 @@ namespace StopWatch
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Seconds");
-            Console.WriteLine("M = Minutes");
+            Console.WriteLine("S = Seconds => 10s = 10 seconds");
+            Console.WriteLine("M = Minutes => 1m = 1 minute");
             Console.WriteLine("0 = Leave StopWatch");
             Console.WriteLine("How many time you wanna count?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+
         }
         static void Start(int time)
         {
